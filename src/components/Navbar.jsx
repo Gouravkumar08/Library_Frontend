e8fa361d14c686f38847a61e3ca1dfaad3235831
Gourav Kumar <raj11gourav44@gmail.com>
2025-05-16 15:10:36 +0530

    commented some feature that is not required at this time

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export function Navbar() {
   return (
     <nav className="flex border border-b-zinc-300 space-x-8 items-center pl-3 py-3 bg-gradient-to-br from-blue-500  to-gray-black justify-evenly">
       <Link to="/" className="text-red-50 text-2xl text-center font-semibold hover:scale-105 duration-100">Home</Link>
-      <Link to="/" className="text-red-50 text-2xl text-center font-semibold hover:scale-105 duration-100">Add book</Link>
+      {/* <Link to="/" className="text-red-50 text-2xl text-center font-semibold hover:scale-105 duration-100">Add book</Link> */}
     </nav>
   );
 }
